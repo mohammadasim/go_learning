fbd5aa4aec27eea9d29721f1ab20eb7f99152b03
Mohammad Ayub <mohammad.ayub@Mohammads-Mini.lan>
2020-02-11 08:08:14 +0000

    working version of anagramsCheck

--- unit3/excr3.12/main.go
+++ unit3/excr3.12/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 )
 
-func anagramsCheck(a string, b string) int {
+func anagramsCheck(a string, b string) bool {
 	// A function to check if the two imput strings are anagrams
+	// change both strings to lower case
 	aString := strings.ToLower(a)
 	bString := strings.ToLower(b)
+
+	// Change both strings to string slices
 	aSplitString := strings.Split(aString, "")
 	bSplitString := strings.Split(bString, "")
 
+	//Sort both string slices
 	sort.Strings(aSplitString)
 	sort.Strings(bSplitString)
-	fmt.Println(aSplitString)
-	fmt.Println(bSplitString)
 
-	return strings.Compare(aString, bString)
+	//change back the sorted string slices to strings
+	aSortedString := strings.Join(aSplitString, ",")
+	bSortedString := strings.Join(bSplitString, ",")
+
+	return aSortedString == bSortedString
 }
 
 func main() {
-	anagramsCheck("adezxnobc", "cqrabegkipqde")
+	println(anagramsCheck("abcd", "bca"))
 }
